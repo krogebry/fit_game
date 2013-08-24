cec50f6d2dd276256e018555a6fe88ebf4ceddcf
krogebry <krogebry@localhost.localdomain>
2013-08-24 10:53:12 -0700

    Adding files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitGame::Application.config.secret_key_base = 'd24260a1349339766091a4e2566aaee9dbde43cd772d0a9ddd4a91275a7982031d9b950577116a54d319dc994a920a0fd7298ecb1c18705012b84fb14e329e04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FitGame::Application.config.session_store :cookie_store, key: '_fit_game_session'
